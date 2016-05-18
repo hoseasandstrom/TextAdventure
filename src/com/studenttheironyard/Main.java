65b07bfcdddfff334ef42eca1c0dd3d357963171
hoseasandstrom <Lakyn4ever>
2016-05-18 11:53:25 -0400

    Start Text Adventure

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -6,39 +6,16 @@ import java.util.Scanner;
 
 public class Main {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
 	    System.out.println("Welcome traveller");
-        System.out.println("What is your name?");
 
-        Scanner scanner = new Scanner(System.in);
+        Player player = new Player();
 
-        String name = scanner.nextLine();
-        System.out.println("Welcome, " + name);
-
-        System.out.println("Choose your weapon [sword/mace]");
-        String weapon = scanner.nextLine();
-
-        if (weapon.equalsIgnoreCase("sword")) {
-            System.out.println("Sword is a fine choice!");
-        }
-        else if (weapon.equalsIgnoreCase("mace")) {
-            System.out.println("Mace is a fine choice!");
-        }
-        else {
-            throw new Exception("Ivalid weapon");
-        }
-        System.out.println("Choose your location {forrest/tunnel");
-        String location = scanner.nextLine();
-
-        if (location.equalsIgnoreCase("forest")){
-            System.out.println("Entering forest...");
-        }
-        else if (location.equalsIgnoreCase("tunnel")) {
-            System.out.println("Entering tunnel...");
-        }
-        else {
-            throw new Exception("Invalid location!");
-        }
+        player.chooseName();
+        player.chooseWeapon();
+        player.chooseLocation();
 
 
     }
