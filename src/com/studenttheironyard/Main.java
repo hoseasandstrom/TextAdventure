55195bcd1c27e4454015d76340156d37f6d5b420
hoseasandstrom <Lakyn4ever>
2016-05-18 11:31:40 -0400

    Start Text Adventure

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -1,8 +1,45 @@
 package com.studenttheironyard;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+	    System.out.println("Welcome traveller");
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        System.out.println("Welcome, " + name);
+
+        System.out.println("Choose your weapon [sword/mace]");
+        String weapon = scanner.nextLine();
+
+        if (weapon.equalsIgnoreCase("sword")) {
+            System.out.println("Sword is a fine choice!");
+        }
+        else if (weapon.equalsIgnoreCase("mace")) {
+            System.out.println("Mace is a fine choice!");
+        }
+        else {
+            throw new Exception("Ivalid weapon");
+        }
+        System.out.println("Choose your location {forrest/tunnel");
+        String location = scanner.nextLine();
+
+        if (location.equalsIgnoreCase("forest")){
+            System.out.println("Entering forest...");
+        }
+        else if (location.equalsIgnoreCase("tunnel")) {
+            System.out.println("Entering tunnel...");
+        }
+        else {
+            throw new Exception("Invalid location!");
+        }
+
+
     }
 }
